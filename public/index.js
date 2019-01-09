c54865d03577d81e888deeff7cb0c49b2b4a23e7
Alan Houston <alanhouston5000@gmail.com>
2019-01-08 20:39:39 -0600

    list products

--- public/index.js
+++ public/index.js
@@ -1,1 +1,15 @@
 //Changing!
+//filter() for search 
+
+//document.body.innerHTML = "whatever"
+
+'use strict';
+
+const search=()=>{
+    const searchInput = document.getElementById("searchBox");
+    console.log(searchInput.value);
+}
+
+document.getElementById("productList").innerHTML = products.map(item=>`<li>${item.name}</li>`)
+        
+    
