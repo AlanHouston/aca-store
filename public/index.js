6e02368245750ec5dae4d5098c88887da897698b
Alan Houston <alanhouston5000@gmail.com>
2019-01-08 20:54:35 -0600

    comma situation fix

--- public/index.js
+++ public/index.js
@@ -7,9 +7,9 @@
 
 const search=()=>{
     const searchInput = document.getElementById("searchBox");
-    console.log(searchInput.value);
+    const searchResults = products.filter(item=>item.name = searchInput)
+    // document.getElementById("showSearchResults").innerHTML = searchResults;
 }
 
-document.getElementById("productList").innerHTML = products.map(item=>`<li>${item.name}</li>`)
-        
-    
+document.getElementById("productList").innerHTML = products.map(item=>`<li>${item.name}</li>`).join('')
+
