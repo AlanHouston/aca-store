7891ddd72780db9db2ea9e7d847c6ee994b29c38
Alan Houston <alanhouston5000@gmail.com>
2019-01-10 10:55:38 -0600

    display products clean

--- public/index.js
+++ public/index.js
@@ -1,25 +1,49 @@
 'use strict';
 
-document.getElementById('productList').innerHTML = products.map(item=>`<li>${item.name}</li>`).join('');
+let displayProducts={
+    products: products,
+    display: function(theProducts){
+        document.getElementById('productList').innerHTML = theProducts.map(item=>
+        `<li>
+            <ul>
+                <li><h2>${item.name}<h2></li>
+                <li><img src="${item.imgUrl}" alt="${item.description}"/></li>
+                <li>Rating: ${item.rating}</li>
+                <li>Reviews: ${item.reviews.length}</li>
+                <li>${item.price}</li>
+            </ul>
+        </li>`).join('');    
+    }
+}
 
-let searchBoxInput = document.getElementById('searchBox');
+displayProducts.display(products);
+
+// document.getElementById('productList').innerHTML = products.map(item=>
+//     `<li>
+//         <ul>
+//             <li><h2>${item.name}<h2></li>
+//             <li><img src="${item.imgUrl}" alt="${item.description}"/></li>
+//             <li>Rating: ${item.rating}</li>
+//             <li>Reviews: ${item.reviews.length}</li>
+//             <li>${item.price}</li>
+//         </ul>
+//     </li>`).join('');
 
 let searchHandlers = {
     input: document.getElementById('searchBox'),
+    // formattedInput: this.input.toLowerCase(),
     results: [],
     search: function(){
+        // console.log(this.formattedInput)
         products.filter(item=>{
             if(item.name.includes(this.input.value)){
                 this.results.push(item);
                 this.showResults();
-                console.log(item);
+                // console.log(item);
             }
         })
     },
     showResults: function(){
-        document.getElementById('productList').innerHTML = this.results.map(item=>`<li>${item.name}</li>`).join('');
+        displayProducts.display(this.results)
     }
 }
-
-
-
