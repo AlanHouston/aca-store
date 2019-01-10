eddf9c967d183d79620ae0e5cfa81481681c7a54
Alan Houston <alanhouston5000@gmail.com>
2019-01-10 12:19:47 -0600

    added cart object

--- public/index.js
+++ public/index.js
@@ -1,7 +1,11 @@
 'use strict';
+//sterilize search input
+//make input form
+
+// import searchHandlers from './searchHandlers';
 
 let displayProducts={
-    products: products,
+    // products: products,
     display: function(theProducts){
         document.getElementById('productList').innerHTML = theProducts.map(item=>
         `<li>
@@ -11,24 +15,32 @@ let displayProducts={
                 <li>Rating: ${item.rating}</li>
                 <li>Reviews: ${item.reviews.length}</li>
                 <li>${item.price}</li>
+                <button onclick="displayProducts.showDetails(${item})">Details</button>
+                <br/>
+                <button onclick="cartHandlers.addToCart(${item})">Add to Cart!</button>
+                <select>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                    <option value='4'>4</option>
+                    <option value='5'>5</option>
+                    <option value='6'>6</option>
+                    <option value='7'>7</option>
+                    <option value='8'>8</option>
+                    <option value='9'>9</option>
+                    <option value='10'>10</option>
+                </select>
+                <li id="detailSpace"></li>
             </ul>
         </li>`).join('');    
+    },
+    showDetails: function(theProduct){
+        document.getElementById('detailSpace').innerHTML = theProduct.description
     }
 }
-
 displayProducts.display(products);
 
-// document.getElementById('productList').innerHTML = products.map(item=>
-//     `<li>
-//         <ul>
-//             <li><h2>${item.name}<h2></li>
-//             <li><img src="${item.imgUrl}" alt="${item.description}"/></li>
-//             <li>Rating: ${item.rating}</li>
-//             <li>Reviews: ${item.reviews.length}</li>
-//             <li>${item.price}</li>
-//         </ul>
-//     </li>`).join('');
-
+//if searchhandlers module, start delete here
 let searchHandlers = {
     input: document.getElementById('searchBox'),
     // formattedInput: this.input.toLowerCase(),
@@ -44,6 +56,21 @@ let searchHandlers = {
         })
     },
     showResults: function(){
+        document.getElementById('titleHolder').innerHTML = 'Search Results!'
         displayProducts.display(this.results)
     }
+}
+//if searchhandlers module, end delete here
+
+
+let cartHandlers = {
+    cart: [],
+    addToCart: function(product){
+        this.cart.push(product);
+    },
+    displayCart: function(){
+        document.getElementById('titleHolder').innerHTML = 'Your Cart!'
+        displayProducts.display(this.cart);
+    }
+
 }
