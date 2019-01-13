8406911aff3659c95cbb0c8d8a98ce033eb8b103
Alan Houston <alanhouston5000@gmail.com>
2019-01-13 14:22:31 -0600

    pushing to attemot display overhaul

--- public/index.js
+++ public/index.js
@@ -1,6 +1,13 @@
 'use strict';
+//Fix details, SEE FUNCTION LINE 39
 //sterilize search input, tolowercase
 //make input form
+//after add item to cart, show cart
+//if no cart, display 'no items in cart'
+
+window.onload = function(){
+    displayProducts.display(products);
+}
 
 let displayProducts={
     // products: products,
@@ -15,18 +22,18 @@ let displayProducts={
                 <li>${item.price}</li>
                 <button onclick="displayProducts.showDetails('${item.description}')">Details</button>
                 <br/>
-                <button onclick="cartHandlers.addToCart('${item.id}')">Add to Cart!</button>
+                <button onclick="cartHandlers.addToCart('${item._id}')">Add to Cart!</button>
                 <select>
-                    <option value='1'>1</option>
-                    <option value='2'>2</option>
-                    <option value='3'>3</option>
-                    <option value='4'>4</option>
-                    <option value='5'>5</option>
-                    <option value='6'>6</option>
-                    <option value='7'>7</option>
-                    <option value='8'>8</option>
-                    <option value='9'>9</option>
-                    <option value='10'>10</option>
+                    <option value=1>1</option>
+                    <option value=2>2</option>
+                    <option value=3>3</option>
+                    <option value=4>4</option>
+                    <option value=5>5</option>
+                    <option value=6>6</option>
+                    <option value=7>7</option>
+                    <option value=8>8</option>
+                    <option value=9>9</option>
+                    <option value=10>10</option>
                 </select>
                 <li id="detailSpace"></li>
             </ul>
@@ -35,12 +42,12 @@ let displayProducts={
     showDetails: function(detail){
         console.log(detail)
         document.getElementById('detailSpace').innerHTML = detail;
+        //WRONG - NEEDS TO OCCUPY WHOLE PAGE, NOT JUST POPULATE BELOW, ALSO NEEDS *ALL* INFO
         // let newLi = document.createElement('li');
         // newLi.textContent = detail;
         // document.getElementById('productUl').appendChild(newLi);
     }
 }
-displayProducts.display(products);
 
 let searchHandlers = {
     input: document.getElementById('searchBox'),
@@ -66,11 +73,16 @@ let searchHandlers = {
 let cartHandlers = {
     cart: [],
     addToCart: function(productId){
-        let cartProduct = products.find(item=>item._id === productId);
-        this.cart.push(cartProduct);  
+        let cartProduct = products.filter(item=>item._id == productId)
+        this.cart.push(cartProduct);
     },
     displayCart: function(){
-        document.getElementById('titleHolder').innerHTML = 'Your Cart!'
-        displayProducts.display(this.cart);
+        document.getElementById('titleHolder').innerHTML = 'Your Cart!';
+        let cartCart = [];
+        for (let i=0;i<this.cart.length;i++){
+            cartCart.push(this.cart[i]);
+        }
+        console.log(cartCart[0])
+        // displayProducts.display(cartCart)
     }
 }
